7244aa7de61855aca0a3b054b3a51635b2b02fdb
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-09 15:00:25 +0500

    Automate
    Run this script when setting up new machine.

---
+++ auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Autmate
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-environment.sh && chmod +x kernel-environment.sh && ./kernel-environment.sh
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-compile.sh && chmod +x kernel-compile.sh && ./kernel-compile.sh
