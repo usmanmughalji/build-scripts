6402b9b086e45a1e1b2bee19ccbd379c26d8b999
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-09 14:59:22 +0500

    Manual
    For manually using those scripts it will first download it and gives proper permissions.

---
+++ manual.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-environment.sh && chmod +x kernel-environment.sh
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-compile.sh && chmod +x kernel-compile.sh
