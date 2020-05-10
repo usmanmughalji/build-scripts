013771aff15bc2b437f97f123479a86cf61ee2d5
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-10 05:12:32 +0500

    Added Script For Upload

--- manual.sh
+++ manual.sh
@@ -3,3 +3,5 @@
 wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-environment.sh && chmod +x kernel-environment.sh
 
 wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-compile.sh && chmod +x kernel-compile.sh
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/upload.sh && chmod +x upload.sh
