f8a91526d6c1021c24fea8ea84e8b2100f59b47f
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-10 05:11:31 +0500

    Added Script For Upload

--- auto.sh
+++ auto.sh
@@ -5,3 +5,5 @@
 wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-environment.sh && chmod +x kernel-environment.sh && ./kernel-environment.sh
 
 wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-compile.sh && chmod +x kernel-compile.sh && ./kernel-compile.sh
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/upload.sh && chmod +x upload.sh && ./upload.sh
