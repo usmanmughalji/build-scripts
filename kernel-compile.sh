b27842c25237dbdd8326efe8111ae1c47725ca38
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-06-29 15:49:55 +0500

    Update kernel-compile.sh

--- kernel-compile.sh
+++ kernel-compile.sh
@@ -2,7 +2,7 @@
 
 cd
 
-git clone https://github.com/usmanmughalji/santoni-msm-4.9.git --single-branch --branch 4.9r1 kernel
+git clone https://github.com/usmanmughalji/kernel_xiaomi_santoni.git --single-branch --branch ten-caf kernel
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 kernel/gcc-64 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 kernel/gcc-32 --depth=1
